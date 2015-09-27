aeeef99212bbfcbd7f9b104f65c230ee89c33576
iknite <enrique@iknite.com>
2015-09-27 10:00:38 +0200

    get SECRET_KEY by environment

--- src/config/settings.py
+++ src/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's5a#790_o3bw9!%5nmz+ufu21r9(u0(je5c)0&29deynfm_px$'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 's5a#790_o3bw9!%5nmz+ufu21r9(u0(je5c)0&29deynfm_px$')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
